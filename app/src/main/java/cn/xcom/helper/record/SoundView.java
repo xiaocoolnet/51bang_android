a800f5741abbc16f573544754818706045cfdaa6
Badreamm <hzh494299@gmail.com>
2017-01-17 23:30:20 +0800

    sound update

--- app/src/main/java/cn/xcom/helper/record/SoundView.java
+++ app/src/main/java/cn/xcom/helper/record/SoundView.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaPlayer;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
@@ -12,14 +16,18 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import cn.xcom.helper.R;
 
 /**
  * Created by hzh on 17/1/17.
+ * 需要加入麦克风权限 否则录不到声音 且没有数据
  */
 
 public class SoundView extends RelativeLayout {
+    public static final int COUNT_CODE =111;
     private boolean isShowDelete;
     private View view;
     private TextView countNumTv;
@@ -28,6 +36,20 @@ public class SoundView extends RelativeLayout {
     private AnimationDrawable animationDrawable;
     private boolean isPlaying;
     private MediaPlayer mediaPlayer;
+    private int countNum;
+    private Timer timer;
+    private TimerTask timerTask;
+    private Handler handler = new Handler(Looper.myLooper()){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case COUNT_CODE:
+                    countNumTv.setText(countNum+"");
+                    break;
+            }
+        }
+    };
 
     public SoundView(Context context) {
         super(context);
@@ -45,7 +67,7 @@ public class SoundView extends RelativeLayout {
         view = LayoutInflater.from(context).inflate(R.layout.view_sound, this, true);
         deleteBtn = (ImageView) view.findViewById(R.id.delete_img);
         animationImg = (ImageView) view.findViewById(R.id.animation_img);
-
+        countNumTv = (TextView) view.findViewById(R.id.num_text);
         if (isShowDelete) {
             deleteBtn.setVisibility(View.VISIBLE);
         } else {
@@ -80,6 +102,24 @@ public class SoundView extends RelativeLayout {
 //                    mediaPlayer.release();
                 }
             });
+            mediaPlayer.prepare();
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    countNum = mediaPlayer.getDuration() / 100;
+                    handler.sendEmptyMessage(COUNT_CODE);
+                }
+            });
+            timer = new Timer();
+            timerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    if (countNum > 0) {
+                        countNum--;
+                        countNumTv.setText(countNum);
+                    }
+                }
+            };
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -90,7 +130,7 @@ public class SoundView extends RelativeLayout {
         try {
             mediaPlayer.prepare();
             mediaPlayer.start();
-
+            timer.schedule(timerTask, 0, 1000);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -99,5 +139,11 @@ public class SoundView extends RelativeLayout {
     private void stopPlay() {
         animationDrawable.stop();
         mediaPlayer.stop();
+        if(timer != null){
+            timer.cancel();
+        }
+        if(timerTask!=null){
+            timerTask.cancel();
+        }
     }
 }
